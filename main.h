ee25025399e5874ca30e3f9ac801f58bc7b381db
Olanewton <Jolakanmi01@gmai.lcom>
2022-10-19 15:04:16 -0700

    Task main.h updated

--- main.h
+++ main.h
@@ -1,17 +1,13 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
 /* FLAGS */
-
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
@@ -19,7 +15,6 @@
 #define F_SPACE 16
 
 /* SIZES */
-
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -29,9 +24,7 @@
  * @fmt: The format.
  * @fn: The function associated.
  */
-
 struct fmt
-
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
@@ -45,9 +38,10 @@ struct fmt
  */
 
 typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+		va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
@@ -72,8 +66,9 @@ int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+
 int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
+		char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
@@ -106,9 +101,10 @@ int write_num(int ind, char bff[], int flags, int width, int precision,
 	int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
+
 int write_unsgnd(int is_negative, int ind,
-char buffer[],
-	int flags, int width, int precision, int size);
+	char buffer[],
+		int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
 int is_printable(char);
